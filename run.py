a09af01ff2db2540b65014337624f0f4469ab8d4
shah77pc <73152402+shah77pc@users.noreply.github.com>
2021-11-27 07:06:54 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,17 @@
 import sys
-import os
+sys.path.append("pixray")
+# Simple setup
 import pixray
-import yaml
 
-# example: python run.py cogs/hello_pixray/pixray.yaml
+prompts = "big white"
+drawer = "pixel"
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python run_yaml file.yaml [other settings]")
-        sys.exit(1)
-
-    settings_file = sys.argv.pop(1)
-    with open(settings_file, 'r') as stream:
-      try:
-          base_settings = yaml.safe_load(stream)
-      except yaml.YAMLError as exc:
-          print("YAML ERROR", exc)
-          sys.exit(1)
-
-    # This loads in default settings, but command line can override
-    pixray.reset_settings()
-    pixray.add_settings(**base_settings)
-    settings = pixray.apply_settings()
-    pixray.do_init(settings)
-    pixray.do_run(settings)
-
-if __name__ == '__main__':
-    main()
+# these are good settings for pixeldraw
+pixray.reset_settings()
+pixray.add_settings(prompts=prompts)
+pixray.add_settings(quality="better")
+pixray.add_settings(drawer=drawer)
+pixray.add_settings(display_clear=True)
+settings = pixray.apply_settings()
+pixray.do_init(settings)
+pixray.do_run(settings)
